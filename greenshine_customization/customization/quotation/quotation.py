d4fb8ef62ad9ed760d40b04dea1a87923975d0cb
Dev Tarpara Technologies <dev.tarparatechnologies@gmail.com>
2019-05-31 15:31:01 +0530

    quotation indentation

--- greenshine_customization/customization/quotation/quotation.py
+++ greenshine_customization/customization/quotation/quotation.py
@@ -1,15 +1,15 @@
 @frappe.whitelist()
-    def make_contract(source_name, target_doc="None"):
-        target_doc = get_mapped_doc("Site", source_name,
-            {"Site": {
-                "doctype": "Greenshine Contract",
-                "field_map": {
-                    "customer": "title"
-                }
-            }},target_doc)
-        target_doc.quotation_to = "Site"
-        target_doc.run_method("set_missing_values")
-        target_doc.run_method("set_other_charges")
-        target_doc.run_method("calculate_taxes_and_totals")
+def make_contract(source_name, target_doc="None"):
+    target_doc = get_mapped_doc("Site", source_name,
+        {"Site": {
+            "doctype": "Greenshine Contract",
+            "field_map": {
+                "customer": "title"
+            }
+        }},target_doc)
+    target_doc.quotation_to = "Site"
+    target_doc.run_method("set_missing_values")
+    target_doc.run_method("set_other_charges")
+    target_doc.run_method("calculate_taxes_and_totals")
 
-        return target_doc
+    return target_doc
