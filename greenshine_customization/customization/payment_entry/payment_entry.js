5678745a2ee7ddcf72bcdb1e4cb49433271bcb22
Jigar Tarpara <jigartarpara68@gmail.com>
2019-06-29 14:24:59 +0530

    Chages for auto fetch

--- greenshine_customization/customization/payment_entry/payment_entry.js
+++ greenshine_customization/customization/payment_entry/payment_entry.js
@@ -1,8 +1,43 @@
 frappe.ui.form.on('Payment Entry', {
     refresh: function(frm){
-        console.log("Hello world from refresh")
+        frappe.call({
+            method:"frappe.client.get_value",
+            args: {
+                doctype:"Naming Series Mapping",
+                filters: {
+                    naming_series:frm.doc.naming_series,
+                },
+                fieldname:["payment_type", "mode_of_payment"]
+            }, 
+            callback: function(r) { 
+                console.log(r);
+                if(r.message){
+                    // set the returned value in a field
+                    cur_frm.set_value("payment_type", r.message.payment_type);
+                    cur_frm.set_value("mode_of_payment", r.message.mode_of_payment);
+                }
+            }
+        })
     },
     naming_series: function(frm) {
-        console.log("Hello Owlrd")
-    }
+        frappe.call({
+            method:"frappe.client.get_value",
+            args: {
+                doctype:"Naming Series Mapping",
+                filters: {
+                    naming_series:frm.doc.naming_series,
+                },
+                fieldname:["payment_type", "mode_of_payment"]
+            }, 
+            callback: function(r) { 
+                console.log(r);
+                if(r.message){
+                    // set the returned value in a field
+                    cur_frm.set_value("payment_type", r.message.payment_type);
+                    cur_frm.set_value("mode_of_payment", r.message.mode_of_payment);
+                }
+            }
+        })
+    },
+
 });
--- greenshine_customization/greenshine_customization/doctype/naming_series_mapping/naming_series_mapping.js
+++ greenshine_customization/greenshine_customization/doctype/naming_series_mapping/naming_series_mapping.js
@@ -3,6 +3,13 @@
 
 frappe.ui.form.on('Naming Series Mapping', {
 	refresh: function(frm) {
-
-	}
+		frappe.call({
+			method:"greenshine_customization.greenshine_customization.doctype.naming_series_mapping.naming_series_mapping.get_series",
+			callback:function(r){
+				$.each(r.message, function(key, value){
+					set_field_options(key, value)
+				})
+			}
+		})
+	},
 });
