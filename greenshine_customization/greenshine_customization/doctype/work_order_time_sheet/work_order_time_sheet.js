bcd1c486005baf2d64627426dfc94af4daa0aff0
Dev Tarpara Technologies <dev.tarparatechnologies@gmail.com>
2019-07-05 17:04:24 +0530

    Work oreer timesheet

---
+++ greenshine_customization/greenshine_customization/doctype/work_order_time_sheet/work_order_time_sheet.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Jigar Tarpara and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Work Order Time Sheet', {
+	refresh: function(frm) {
+
+	}
+});
