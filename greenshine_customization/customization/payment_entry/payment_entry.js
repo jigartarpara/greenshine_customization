d2fc2c3cbf87dff2a24fb330f3d52b08c704cd84
Jigar Tarpara <jigartarpara68@gmail.com>
2019-07-31 11:43:13 +0530

    Changes for Payment Entry AUto Mapping

--- greenshine_customization/customization/payment_entry/payment_entry.js
+++ greenshine_customization/customization/payment_entry/payment_entry.js
@@ -1,23 +1,25 @@
 frappe.ui.form.on('Payment Entry', {
     refresh: function(frm){
-        frappe.call({
-            method:"frappe.client.get_value",
-            args: {
-                doctype:"Naming Series Mapping",
-                filters: {
-                    naming_series:frm.doc.naming_series,
-                },
-                fieldname:["payment_type", "mode_of_payment"]
-            }, 
-            callback: function(r) { 
-                console.log(r);
-                if(r.message){
-                    // set the returned value in a field
-                    cur_frm.set_value("payment_type", r.message.payment_type);
-                    cur_frm.set_value("mode_of_payment", r.message.mode_of_payment);
+        if(frm.doc.__islocal) {
+            frappe.call({
+                method:"frappe.client.get_value",
+                args: {
+                    doctype:"Naming Series Mapping",
+                    filters: {
+                        naming_series:frm.doc.naming_series,
+                    },
+                    fieldname:["payment_type", "mode_of_payment"]
+                }, 
+                callback: function(r) { 
+                    console.log(r);
+                    if(r.message){
+                        // set the returned value in a field
+                        cur_frm.set_value("payment_type", r.message.payment_type);
+                        cur_frm.set_value("mode_of_payment", r.message.mode_of_payment);
+                    }
                 }
-            }
-        })
+            })
+        }
     },
     naming_series: function(frm) {
         frappe.call({
